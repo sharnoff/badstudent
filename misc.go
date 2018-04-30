2c5c76f4fa44c4be734ba49820ce824f5246e909
sharnoff <github@max.sharnoff.org>
2018-04-30 12:03:15 -0700

    added CorrectHighest as built-in IsCorrect()

--- misc.go
+++ misc.go
@@ -2,9 +2,10 @@ package badstudent
 
 import (
 	"math"
+	"sort"
 )
 
-// assumes len(outs) == len(targets_)
+// assumes len(outs) == len(targets)
 func CorrectRound(outs, targets []float64) bool {
 	for i := range outs {
 		// rounds to 0 if a number is < 0.5, 1 if > 0.5. Tanh reduces the value to (0, 1)
@@ -16,8 +17,51 @@ func CorrectRound(outs, targets []float64) bool {
 	return true
 }
 
+// for use in CorrectHighest()
+type sortable struct {
+	values []float64
+	indexes []int
+}
+
+func (s sortable) Len() int {
+	return len(s.values)
+}
+func (s sortable) Less(i, j int) bool {
+	return s.values[i] > s.values[j]
+}
+func (s sortable) Swap(i, j int) {
+	s.values[i], s.values[j] = s.values[j], s.values[i]
+	s.indexes[i], s.indexes[j] = s.indexes[j], s.indexes[i]
+	return
+}
+
+// just returns whether or not the largest value in each is the same
+func CorrectHighest(outs, targets []float64) bool {
+	indexes := make([]int, len(outs))
+	for i := range indexes {
+		indexes[i] = i
+	}
+
+	copyOfIndexes := make([]int, len(outs))
+	copy(copyOfIndexes, indexes)
+
+	o := sortable{outs, indexes}
+	t := sortable{targets, copyOfIndexes}
+
+	sort.Sort(o)
+	sort.Sort(t)
+
+	return o.indexes[0] == t.indexes[0]
+}
+
+func TrainForIterations(maxIterations int) func(int, int, float64) bool {
+	return func(iteration, epoch int, lastErr float64) bool {
+		return iteration < maxIterations
+	}
+}
+
 // returns a function that satisfies TrainArgs.RunCondition
-func TrainFor(maxEpochs int) func(int, int, float64) bool {
+func TrainForEpochs(maxEpochs int) func(int, int, float64) bool {
 	return func(iteration, epoch int, lastErr float64) bool {
 		return epoch < maxEpochs
 	}
