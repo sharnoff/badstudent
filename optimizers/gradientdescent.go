39a01d7b60077c5e2f5c757f0cd9397a32f062d7
sharnoff <github@max.sharnoff.org>
2018-05-05 14:24:53 -0700

    insignificant change to optimizers/gradientdescent.go

--- optimizers/gradientdescent.go
+++ optimizers/gradientdescent.go
@@ -1,7 +1,7 @@
 package optimizers
 
 import (
-	"github.com/sharnoff/smartlearning/badstudent"
+	"github.com/sharnoff/badstudent"
 )
 
 type gradientdescent int8
@@ -13,7 +13,7 @@ func GradientDescent() gradientdescent {
 func (g gradientdescent) Run(l *badstudent.Layer, size int, grad func(int) float64, add func(int, float64), learningRate float64) error {
 
 	for i := 0; i < size; i++ {
-		add(i, -1*learningRate*grad(i))
+		add(i, -1 * learningRate * grad(i))
 	}
 
 	return nil
