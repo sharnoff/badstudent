4d4cdface0690fe186a29b8a3a798d08156b43b1
sharnoff <github@max.sharnoff.org>
2018-05-07 19:37:18 -0700

    more multithreading

--- optimizers/gradientdescent.go
+++ optimizers/gradientdescent.go
@@ -2,7 +2,10 @@ package optimizers
 
 import (
 	"github.com/sharnoff/badstudent"
+	"runtime"
 )
+
+const threadSizeMultiplier int = 2
 
 type gradientdescent int8
 
@@ -12,9 +15,14 @@ func GradientDescent() gradientdescent {
 
 func (g gradientdescent) Run(l *badstudent.Layer, size int, grad func(int) float64, add func(int, float64), learningRate float64) error {
 
-	for i := 0; i < size; i++ {
+	threadsPerCPU := 1
+	opsPerThread := runtime.NumCPU()
+
+	f := func(i int) {
 		add(i, -1 * learningRate * grad(i))
 	}
+
+	badstudent.MultiThread(0, size, f, opsPerThread, threadsPerCPU)
 
 	return nil
 }
