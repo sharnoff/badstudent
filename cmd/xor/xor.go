1028ece5b23b9a5237f4df5c36001ec87aeddc64
sharnoff <github@max.sharnoff.org>
2018-08-04 15:26:39 -0700

    reworked cmd/xor to allow easier testing

--- cmd/xor/xor.go
+++ cmd/xor/xor.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/sharnoff/badstudent"
+	bs "github.com/sharnoff/badstudent"
 	"github.com/sharnoff/badstudent/operators"
 	"github.com/sharnoff/badstudent/operators/optimizers"
 
@@ -19,6 +19,124 @@ func format(fs ...float64) (str string) {
 	return
 }
 
+const (
+	statusFrequency int = 100
+	testFrequency int = 20
+
+	// main hyperparameters
+	learningRate float64 = 1.
+	batchSize int = 4
+	maxIterations int = 3000
+
+	// where to save/load the network
+	path string = "xor save"
+)
+
+func train(net *bs.Network, dataset [][][]float64) {
+	trainData, err := bs.DataCh(dataset, true)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	testData, err := bs.DataCh(dataset, false)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	res := make(chan bs.Result)
+
+	args := bs.TrainArgs{
+		Data:         trainData,
+		TestData:     testData,
+		ShouldTest:   bs.TestEvery(testFrequency, len(dataset)),
+		SendStatus:   bs.Every(statusFrequency),
+		Batch:        bs.BatchEvery(batchSize),
+		RunCondition: bs.TrainUntil(maxIterations),
+		LearningRate: bs.ConstantRate(learningRate),
+		// IsCorrect: bs.CorrectRound,
+		// CostFunc:  bs.SquaredError(false),
+		Results: res,
+		Err:     &err,
+	}
+
+	fmt.Println("Starting training...")
+	go net.Train(args)
+	fmt.Println("Iteration, Status Cost, Status Percent, Test Cost, Test Percent")
+
+	// statusCost, statusPercent, testCost, testPercent
+	results := make([]float64, 4)
+	previousIteration := -1
+
+	for r := range res {
+
+		if r.Iteration > previousIteration && previousIteration >= 0 {
+			fmt.Printf("%d, %s\n", previousIteration, format(results...))
+
+			results = make([]float64, len(results))
+		}
+
+		if r.IsTest {
+			results[2] = r.Avg
+			results[3] = r.Percent
+		} else {
+			results[0] = r.Avg
+			results[1] = r.Percent
+		}
+
+		previousIteration = r.Iteration
+	}
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("%d, %s\n", previousIteration, format(results...))
+	fmt.Println("Done training!")
+}
+
+func test(net *bs.Network, dataset [][][]float64) {
+	testData, err := bs.DataCh(dataset, false)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Testing...")
+	_, _, err = net.Test(testData, bs.SquaredError(true), bs.CorrectRound, len(dataset))
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func save(net *bs.Network) {
+	fmt.Println("Saving...")
+	path := "xor save"
+	if err := net.Save(path, true); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Done!")
+}
+
+func load() (net *bs.Network) {
+
+	fmt.Println("Loading...")
+	path := "xor save"
+	types := map[string]bs.Operator{
+		"input":                 operators.Neurons(optimizers.GradientDescent()),
+		"hidden layer neurons":  operators.Neurons(optimizers.GradientDescent()),
+		"hidden layer logistic": operators.Logistic(),
+		"output neurons":        operators.Neurons(optimizers.GradientDescent()),
+		"output logistic":       operators.Logistic(),
+	}
+	// no auxiliary information necessary
+	var err error
+	if net, err = bs.Load(path, types, nil); err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Done!")
+
+	return
+}
+
 func main() {
 	dataset := [][][]float64{
 		{{-1, -1}, {0}},
@@ -27,25 +145,11 @@ func main() {
 		{{1, 1}, {0}},
 	}
 
-	// these are the main adjustable variables
-	statusFrequency := 100
-	testFrequency := 20
-
-	// primary set of hyperparameters
-	learningRate := 1.0
-	batchSize := 4
-	maxIterations := 3000 // 750 epochs
-
-	// alternate set that works
-	// learningRate := 1.0
-	// batchSize := 1
-	// maxIterations := 1200
-
-	net := new(badstudent.Network)
+	net := new(bs.Network)
 	fmt.Println("Setting up network...")
 	{
 		var err error
-		var l, hl *badstudent.Node
+		var l, hl *bs.Node
 
 		if l, err = net.Add("input", operators.Neurons(optimizers.GradientDescent()), 2); err != nil {
 			panic(err.Error())
@@ -73,109 +177,11 @@ func main() {
 	}
 	fmt.Println("Done!")
 
-	trainData, err := badstudent.DataCh(dataset, true)
-	if err != nil {
-		panic(err.Error())
-	}
+	train(net, dataset)
+	test(net, dataset)
+	save(net)
+	net = load()
+	train(net, dataset)
+	test(net, dataset)
 
-	testData, err := badstudent.DataCh(dataset, false)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	res := make(chan badstudent.Result)
-
-	args := badstudent.TrainArgs{
-		Data:         trainData,
-		TestData:     testData,
-		ShouldTest:   badstudent.TestEvery(testFrequency, len(dataset)),
-		SendStatus:   badstudent.Every(statusFrequency),
-		Batch:        badstudent.BatchEvery(batchSize),
-		RunCondition: badstudent.TrainUntil(maxIterations),
-		LearningRate: badstudent.ConstantRate(learningRate),
-		// IsCorrect: badstudent.CorrectRound,
-		// CostFunc:  badstudent.SquaredError(false),
-		Results: res,
-		Err:     &err,
-	}
-
-	if &args == nil {
-		fmt.Println("Just here to keep the compiler from complaining about unused variables :)")
-	}
-
-	fmt.Println("Starting training...")
-	{
-		go net.Train(args)
-		fmt.Println("Iteration, Status Cost, Status Percent, Test Cost, Test Percent")
-
-		// statusCost, statusPercent, testCost, testPercent
-		results := make([]float64, 4)
-		previousIteration := -1
-
-		for r := range res {
-
-			if r.Iteration > previousIteration && previousIteration >= 0 {
-				fmt.Printf("%d, %s\n", previousIteration, format(results...))
-
-				results = make([]float64, len(results))
-			}
-
-			if r.IsTest {
-				results[2] = r.Avg
-				results[3] = r.Percent
-			} else {
-				results[0] = r.Avg
-				results[1] = r.Percent
-			}
-
-			previousIteration = r.Iteration
-		}
-
-		if err != nil {
-			panic(err.Error())
-		}
-
-		fmt.Printf("%d, %s\n", previousIteration, format(results...))
-	}
-	fmt.Println("Done training!")
-
-	fmt.Println("Testing...")
-	{
-		_, _, err = net.Test(testData, badstudent.SquaredError(true), badstudent.CorrectRound, len(dataset))
-	}
-
-	fmt.Println("Saving...")
-	{
-		path := "xor save"
-		if err := net.Save(path, true); err != nil {
-			panic(err.Error())
-		}
-	}
-	fmt.Println("Done!")
-
-	fmt.Println("Loading again...")
-	{
-		path := "xor save"
-		types := map[string]badstudent.Operator{
-			"input":                 operators.Neurons(optimizers.GradientDescent()),
-			"hidden layer neurons":  operators.Neurons(optimizers.GradientDescent()),
-			"hidden layer logistic": operators.Logistic(),
-			"output neurons":        operators.Neurons(optimizers.GradientDescent()),
-			"output logistic":       operators.Logistic(),
-		}
-		// no auxiliary information necessary
-		var err error
-		if net, err = badstudent.Load(path, types, nil); err != nil {
-			panic(err.Error())
-		}
-	}
-	fmt.Println("Done!")
-
-	fmt.Println("Testing, once more...")
-	{
-		_, _, err = net.Test(testData, badstudent.SquaredError(true), badstudent.CorrectRound, len(dataset))
-		if err != nil {
-			panic(err.Error())
-		}
-	}
 }
